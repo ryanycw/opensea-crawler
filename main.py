e08e780c543134aa51684972ef39211a3adb228e
ryanycw <ryan0213.tem04@nctu.edu.tw>
2022-12-19 15:14:19 +0800

    remove unuse lines, crawling structure

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from itertools import zip_longest
 
+def convertText2Int(input):
+    intList = input.split(',')
+    output = 0
+    for id, element in enumerate(intList):
+        output += int(element)*pow(10,3*(len(intList)-id-1))
+    
+    return output
+
 def main():
     url = 'https://opensea.io/collection/0n1-force'
 
@@ -11,6 +19,7 @@ def main():
     chrome_options.add_argument("--incognito")  
     driver = webdriver.Chrome(ChromeDriverManager().install(),options=chrome_options)
     driver.get(url)
+    totalMints = convertText2Int(driver.find_element_by_xpath('//*[@id="__next"]/div[1]/main/div/div/div[2]/div/div/div[2]/div[2]/div/span').text.split(' ')[0])
 
     soup = BeautifulSoup(driver.page_source, 'lxml')
     filter = soup.find('div', {'data-testid': 'SearchFilter'})
@@ -19,8 +28,7 @@ def main():
     output = []
 
     for i, property in enumerate(properties):
-        
-        propertyName = property.find('span')
+        propertyName = property.find('div', {'class': 'Blockreact__Block-sc-1xf18x6-0 gLNyor'})
         options = property.find_all('button', {'class': 'UnstyledButtonreact__UnstyledButton-sc-ty1bh0-0 btgkrL StringTraitFilter--item'})
 
         itemCol = []
@@ -36,16 +44,14 @@ def main():
             amountCol.append(optionCnt.string)
 
         amountCal = [int(integer) for integer in amountCol[1:]]
-        percentageCol.extend([round(integer/sum(amountCal)*100,2) for integer in amountCal])
+        percentageCol.extend([round(integer/totalMints*100,2) for integer in amountCal])
         output.extend([itemCol, amountCol, percentageCol])
 
     exportData = zip_longest(*output, fillvalue = '')
-    with open(f"{url.split('/')[-1]}Rariry.csv", 'w', newline='') as outputFile:
+    with open(f"data/{url.split('/')[-1]}-Rariry.csv", 'w', newline='') as outputFile:
         wr = csv.writer(outputFile)
         wr.writerows(exportData)
         outputFile.close()
-    
-    #time.sleep(10)
 
 if __name__ == "__main__":
     main()
